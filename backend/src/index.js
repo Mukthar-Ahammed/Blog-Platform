9f1abf06f2613e81c93ba0a1d7a7fb9798e75409
Mukthar Ahammed <mukthar2004@gmail.com>
2025-11-13 16:26:18 +0530

    deployment3

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", 
-      "https://blog-platform-lwzn.vercel.app/" 
+      "https://blog-platform-lwzn.vercel.app" 
     ],
     credentials: true,
   })
