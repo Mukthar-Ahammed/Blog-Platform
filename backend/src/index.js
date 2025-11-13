167f7bbe12a469b875434ca77b7b4ff15954e34e
Mukthar Ahammed <mukthar2004@gmail.com>
2025-11-13 16:10:36 +0530

    Initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", 
-      "https://your-frontend-name.vercel.app" 
+      "https://blog-platform-lwzn.vercel.app/" 
     ],
     credentials: true,
   })
